064e91610ec945e0d7631f4000646f9a1a54588c
minecoby <minecoby@naver.com>
2024-11-03 16:23:04 +0900

    유효성검사 추가

--- user/user_schema.py
+++ user/user_schema.py
@@ -14,3 +14,13 @@ class NewUserForm(BaseModel):
             raise HTTPException(status_code=422, detail="모든 항목을 입력해주세요.")
         return v
     
+    @field_validator('password')
+    def validate_password(cls,v):
+        if len(v) < 8 or not any(char.isdigit() for char in v) or not any(char.isalpha() for char in v):
+            raise HTTPException(status_code=422, detail="비밀번호는 8자리 이상, 영문과 숫자를 포함하여 입력해주세요.")
+        return v
+    
+    @field_validator('nickname')
+    def validate_nickname(cls,v):
+        if len(v) < 3:
+            raise HTTPException(status_code=422, detail="닉네임은 최소 3글자 이상으로 입력해주세요.")
