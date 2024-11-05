4ead25f2ed84bd1813b0d0a5545c444a4d212263
minecoby <minecoby@naver.com>
2024-11-05 09:50:53 +0900

    이메일 인증을위한 모델 생성

--- user/user_model.py
+++ user/user_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String,Boolean
+from sqlalchemy import Column, Integer, String,Boolean,DateTime
 from user_db import user_Base
 
 class User(user_Base):
@@ -11,4 +11,12 @@ class User(user_Base):
     name = Column(String(15), nullable=False)
     nickname = Column(String(15), nullable=False, unique= True, index=True)
     is_mentor = Column(Boolean)
+    
+class VerifiedEmail(user_Base):
+    __tablename__ = "VerifiedEmails"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String(20), nullable=False, index=True)
+    email = Column(String(40), nullable=False, index=True)
+    code = Column(String(15), nullable=False)
+    created_at = Column(DateTime)
     
