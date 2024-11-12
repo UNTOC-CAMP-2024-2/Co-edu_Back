59546db6e57db65edc793dd22f42e01a273f06bc
minecoby <minecoby@naver.com>
2024-11-12 13:43:08 +0900

    Revert "모델 및 데이터베이스 생성"
    This reverts commit 3f4b62c1921b569cb15efcd29f50860878693719.

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from user.user_router import router as user_router
 from user.user_db import user_Base,user_engine
-
-from classroom.cs_router import router as cs_router
-from classroom.cs_db import cs_Base,cs_engine
-
-from assignment.assign_router import router as assign_router
 import smtplib
 from email.mime.text import MIMEText
 from variable import *
@@ -23,11 +18,7 @@ smtp.login(EMAIL, EMAIL_PASSWORD)
 
 app = FastAPI()
 user_Base.metadata.create_all(bind=user_engine)
-cs_Base.metadata.create_all(bind=cs_engine)
 app.include_router(user_router, tags=["user"])
-app.include_router(cs_router, tags=["classroom"])
-app.include_router(assign_router, tags=["assignment"])
-
 
 @app.get("/")
 async def init():
